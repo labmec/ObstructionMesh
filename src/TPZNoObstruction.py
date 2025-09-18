e92a6e6d5c96b90c7a22baa0160c6cacc013aa13
CarlosPuga14 <carlospuga1404@gmail.com>
2025-09-18 19:59:45 -0300

    refac: dataclass removed. Rectangular typology deleted.

--- src/TPZNoObstruction.py
+++ src/TPZNoObstruction.py
@@ -3,18 +3,16 @@ Class to create a module without obstructions
 
 Created by Carlos Puga: 01/13/2024
 """
-
 #%% ****************** 
 #   IMPORTED MODULES
 #   ******************
-from dataclasses import dataclass, field
 import gmsh
 
 from src.TPZModuleTypology import TPZModuleTypology
+
 #%% ****************** 
 #   CLASS DEFINITION
 #   ******************
-@dataclass
 class TPZNoObstruction(TPZModuleTypology):
     """
     This class creates a module without obstrucitons
@@ -23,82 +21,42 @@ class TPZNoObstruction(TPZModuleTypology):
     to generates the module body and insert the obstruction.
 
     Fields: 
-        - module_typology: information about the type 
-        of module to be constructed. Currently is only availabe rectangular 
-        modules. More information are required: 
-            * Rectangular -> {dx: length on x-direction, dy: length on y-direction}
-
-        - id: obstruction volume's identification (provided by the class itself)
+        - length: module length
+        - lc: mesh size (gmsh requirement)
     """
 #   ****************** 
 #      INITIALIZOR
 #   ******************  
-    _module_typology: tuple[str, dict]
-    _id: int = field(init=False)
+    def __init__(self, length:float, lc:float, radius:float) -> None:
+        super().__init__(length=length, lc=lc, radius=radius)
 
-    def __post_init__(self):
+        self.__post_init__()
+
+        return
+
+    def __post_init__(self) -> None:
         self.CreateDomain()
-    
-#   ****************** 
-#   GETTERS & SETTERS
-#   ******************  
-    @property
-    def module_typology(self)->tuple[str, dict]: return self._module_typology
-    @module_typology.setter
-    def module_typology(self, Module)->None: self._module_typology = Module
-
-    @property
-    def id(self)->int: return self._id
-    @id.setter
-    def id(self, ID)->None: self._id = ID
+        return
 
 #   ****************** 
 #        METHODS
 #   ******************  
-    def CheckTypology(self)->None:
-        """
-        Checks whether the module is rectangular or circular 
-        """
-        m_type, m_characteristics = self.module_typology
-        keys = list(m_characteristics.keys())
-
-        if m_type == 'Rectangular':
-            if not 'dx' in keys and not 'dy' in keys:
-                self.DebugStop('ERROR: Not enough information to create the box!')
-
-            dx = m_characteristics['dx']
-            dy = m_characteristics['dy']
-
-            self.CreateBox(dx, dy)
-
-        elif m_type == 'Circular':
-            if not 'radius' in keys:
-                self.DebugStop('ERROR: Not enough information to create the box!')
-
-            radius = m_characteristics['radius']
-
-            self.CreateCylinder(radius)
-            
-        else:
-            self.DebugStop(f"ERROR: I'm sorry the typology {m_type} has not been implemented yet")
-
-
-    def CreateDomain(self)->None:
+    def CreateDomain(self) -> None:
         """
         Generates the module with a circular obstruction on gmsh.
         """
-        self.CheckTypology()
+        self.CreateCylinder()
 
-        domain_surfaces = self.surfaces + [self.obstruction_face]
+        domainSurfaces = self.fSurfaces + [self.fObstructionSurface]
 
         # creating the module volume
-        surface_loop = gmsh.model.occ.addSurfaceLoop(domain_surfaces)
-        domain_volume = gmsh.model.occ.addVolume([surface_loop])
+        surfaceLoop = gmsh.model.occ.addSurfaceLoop(domainSurfaces)
+        self.fVolumeID = gmsh.model.occ.addVolume([surfaceLoop])
 
-        self.id = domain_volume
+        return
 
-    def Move(self, dx:float, dy:float, dz:float)->None:
+    def Move(self, dx:float, dy:float, dz:float) -> None:
         """
         Moves the module (dx, dy, dz)
         """
-        gmsh.model.occ.translate([ (3,self.id)], dx, dy, dz)
+        gmsh.model.occ.translate([ (3,self.fVolumeID)], dx, dy, dz)
